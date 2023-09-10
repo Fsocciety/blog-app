5d732cef779157aa9c217f4f2854121d4309ae42
Uros <dixypixy007@gmail.com>
2023-09-10 10:52:57 +0200

    database connection modified

--- api/db.js
+++ api/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "blog",
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
   password: process.env.DB_PASSWORD,
 });
 
